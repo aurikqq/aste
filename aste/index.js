1b3b07d60f7638ed670d5d48c67fc42865b89ca5
aurikqq <awergiony@gmail.com>
2026-08-01 23:16:54 +0300

    top bar now moves to the corner when page is scrolled, added shadows for top bar and cards and a underlay for sections titles

--- aste/index.js
+++ aste/index.js
@@ -1,53 +1,15 @@
-const model = [
-    {type: 'title', value: "Hello World from JS"},
-    {type: 'text', value: 'here we go with some text'},
-    {type: 'columns', value: [
-        '111111',
-        '222222',
-        '333333'
-    ]}
-]
 
-const site = document.querySelector('#site')
-
-model.forEach ( block => {
-    let html = ''
-
-    if (block.type === 'title') {
-        html = title(block)
-    } else if (block.type === 'text') {
-        html = text(block)
-    } else if (block.type === 'columns') {
-        html = columns(block)
+$(document).scroll(function() {
+    if($(document).scrollTop() >= 100) {
+        $('.topbar').addClass('no-after');
+        $('.topbar').addClass('no-before');
+        $('.topbar').addClass('scroll');
+        $('.logo').toggle(false);
+        
+    } else {
+        $('.topbar').removeClass('no-after');
+        $('.topbar').removeClass('no-before');
+        $('.topbar').removeClass('scroll');
+        $('.logo').toggle(true);
     }
-
-    site.insertAdjacentHTML('beforeend', html)
-})
-
-function title(block) {
-    return `
-        <div class="row">
-            <div class="col-sm">
-                <h1>Hello World!</h1>
-            </div>
-        </div>
-    `
-}
-
-function text(block) {
-    return `
-        <div class="col-sm">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti ab molestias debitis saepe molestiae atque.</p>
-        </div>
-    `
-}
-
-function columns(block) {
-    const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
-
-    return `
-        <div class="row">
-            ${html.join('')}
-        </div>
-    `
-}
+});
